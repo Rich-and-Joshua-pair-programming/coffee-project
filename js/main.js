89162987de3a69ef5b9f4ddb8c4f052c3214d72c
Richard Kim <richardjkim7@gmail.com>
2022-09-19 10:06:34 -0500

    almost completed project

--- js/main.js
+++ js/main.js
@@ -1,52 +1,75 @@
 "use strict"
 
-/*We want to submit the form with just the dropdown button*/
-
-/*Collecting Form Data
-* 1)how to collect data form text boxes
-*
-*
-* */
-
-// document.getElementById()
-
-
-/*This is making our main list of coffees*/
 function renderCoffee(coffee) {
-    let html = '<div class="coffee">';
-    // html += '<div>' + coffee.id + '</div>';
-    html += '<h2>' + coffee.name + '</h2>';
-    html += '<p>' + coffee.roast + '</p>';
-    html += '</div>';
+    let html = '<tr class="coffee">';
+    // html += '<td>' + coffee.id + '</td>';
+    html += '<div class="coffee-name">' + coffee.name + '</div>';
+    html += '<p class="coffee-roast">' + coffee.roast + '</p>';
+    html += '</tr>';
 
     return html;
-    // console.log(html);
 }
 
+//without this code nothing is printed to webpage
 
-/*This function displays the array in ascending order*/
 function renderCoffees(coffees) {
     let html = '';
     for(let i = 0; i < coffees.length; i++) {
         html += renderCoffee(coffees[i]);
     }
+    console.log(html);
     return html;
 }
 
-
-/*This is what is controlling the list of coffees by Roast Type*/
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
     let selectedRoast = roastSelection.value;
     let filteredCoffees = [];
     coffees.forEach(function(coffee) {
-        if (coffee.roast === selectedRoast) {
+        if (coffee.roast === selectedRoast && coffeeSearch.value === "") {
             filteredCoffees.push(coffee);
-        } else if (selectedRoast === "all") {
+        } else if (selectedRoast === "all" && coffeeSearch.value === "") {
             filteredCoffees.push(coffee);
+        } else if (coffee.roast === selectedRoast && coffeeSearch.value.toLowerCase() === coffee.name.toLowerCase()) {
+            filteredCoffees.push(coffee);
+        } else if (coffee.name.includes("City") && selectedRoast === "all") {
+            filteredCoffees.push(coffee);
+            // } else if (coffee.name.includes("City") && selectedRoast === "light") {
+            //     //City medium is showing up here for some reason...
+            // filteredCoffees.push(coffee);
+            // if (filteredCoffees.length === 3) {
+            //     filteredCoffees.pop();
+            // }
         }
     });
-    showMeTheCoffee.innerHTML = renderCoffees(filteredCoffees);
+    tbody.innerHTML = renderCoffees(filteredCoffees);
+}
+
+function myFunction() {
+    // alert(`You selected ${roastSelection.value}`);
+    let selectedRoast1 = roastSelection.value;
+    let filteredCoffees1 = [];
+    coffees.forEach(function(coffee) {
+        if (coffee.roast === selectedRoast1 && coffeeSearch.value === "") {
+            filteredCoffees1.push(coffee);
+        } else if (selectedRoast1 === "all" && coffeeSearch.value === "") {
+            filteredCoffees1.push(coffee);
+        } else if (coffee.roast === selectedRoast1 && coffeeSearch.value.toLowerCase() === coffee.name.toLowerCase()) {
+            filteredCoffees1.push(coffee);
+        } else if (coffee.name.includes("City") && selectedRoast1 === "all") {
+            filteredCoffees1.push(coffee);
+            // } else if (coffee.name.includes("City") && selectedRoast1 === "light") {
+            //     filteredCoffees1.push(coffee);
+            //     if (filteredCoffees1.length === 3) {
+            //         filteredCoffees1.pop();
+            //     }
+        }
+    });
+    tbody.innerHTML = renderCoffees(filteredCoffees1);
+}
+
+function addNewCoffee() {
+    console.log(coffees.push(coffeeAdd.value));
 }
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
@@ -67,22 +90,48 @@ let coffees = [
     {id: 14, name: 'French', roast: 'dark'},
 ];
 
+// use .find/.filter to find types in the array
 
-//This is a variable that pulls the id of #coffees from the empty div
-let showMeTheCoffee = document.querySelector('#coffees');
+// an array of objects with name and roast
 
+// write code to push new coffee name and roast onto existing array
 
-//Pulls all children nodes (options) of the select element
-let roastSelection = document.getElementById("roast-selection");
-console.log(roastSelection);
+let tbody = document.querySelector('#coffees');
+let submitButton = document.querySelector('#submit');
+let roastSelection = document.querySelector('#roast-selection');
+let searchForCoffee = document.querySelector('#coffee-search');
 
+tbody.innerHTML = renderCoffees(coffees);
 
-//This will allow the list to update With the dropdown alone.
-roastSelection.addEventListener("change", updateCoffees);
+submitButton.addEventListener('click', updateCoffees);
 
+searchForCoffee.addEventListener('keyup', updateCoffees);
 
-/*This will activate the function of renderCoffees the empty div
-* displaying our coffees onto the page */
-showMeTheCoffee.innerHTML = renderCoffees(coffees);
+console.log(document.forms);
 
-//Rich received
+let coffeeSearch = document.getElementById('coffee-search');
+
+let roastSelection1 = document.querySelector('#roast-selection1');
+
+let coffeeAdd = document.querySelector('#coffeeAdd');
+
+let submitNewCoffee = document.querySelector('#submitNewCoffee');
+
+submitNewCoffee.addEventListener('click', addNewCoffee);
+
+// let submitCoffee = function (event) {
+//     alert(`You are searching for ${coffeeSearch.value}`);
+// }
+//
+// submitButton.addEventListener('click', submitCoffee);
+
+// 1. dropdown menu for roast: all, light, dark etc
+// 2. Search bar for coffee name (includes coffee name too)
+// 3. A separate form to add new roast/coffee name at the beginning of an array
+
+// Need to create two forms - first input is drop and second input is text
+// Coffee name is displayed in big letters first and then next is type of roast in small letters
+// Don't need to click submit button
+// First form sorts existing items in the array and second form purely adds new items to the array
+
+// roast 'all' should all elements in the object
